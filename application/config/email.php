ea7b8c32a6df0f4bc45cd1a3b8a3ecec7b174a1e
Maulana Ichwan Anshory <64744726+masiwangs@users.noreply.github.com>
2023-05-04 02:25:54 +0700

    update email smtp

--- application/config/email.php
+++ application/config/email.php
@@ -3,12 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config = array(
     'protocol' => 'smtp',
-    'smtp_host' => 'mail.mahameru.wonosobokab.go.id',
-    'smtp_port' => '465',
+    'smtp_host' => 'smtp-relay.sendinblue.com',
+    'smtp_port' => '587',
     'smtp_crypto' => 'ssl',
     'smtp_timeout' => '4',
     'smtp_user' => 'support@mahameru.wonosobokab.go.id',
-    'smtp_pass' => 'M4h4m3ruW0n0s0b02022%',
+    'smtp_pass' => 'xsmtpsib-a13b8c4057ff5ae0ca53cc59d8df2b95a7af08943f7175acfe83c9be130e5e4d-8Uc725IBVsPbgEfQ',
     'mailtype' => 'html',
     'charset' => 'iso-8859-1',
     'wordwrap' => TRUE
